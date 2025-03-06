328c4994c0b9b2e90bcf803dfb1abeaed57790e9
Malanaa <abdullahdotpy@gmail.com>
2025-03-05 22:07:37 -0600

    I don't really know what I am yet

---
+++ Kindle.cpp
@@ -0,0 +1,61 @@
+#include <string>
+#include <vector>
+
+class Kindle {       
+
+    std::string kindle;
+    InitialCondition init;
+    Health health;
+    Hygiene hygiene;
+    Mood mood;
+    Enegry energy;
+    Temper temper;
+    Ego ego;
+    Intelligence intelligence;
+    BoundryCondition bound;
+
+
+    public:
+    // pass
+
+};
+
+
+
+// Awared Conditions 
+
+class InitialCondition {
+
+};
+
+class Health {
+
+};
+
+class Hygiene {
+
+};
+
+class Mood {
+
+};
+
+class Enegry {
+
+};
+
+class Temper {
+
+};
+
+class Ego {
+
+};
+
+class Intelligence {
+
+};
+
+class BoundryCondition {
+
+};
